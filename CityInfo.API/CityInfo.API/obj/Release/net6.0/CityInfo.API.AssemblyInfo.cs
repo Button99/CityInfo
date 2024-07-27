0a344fde43d5d608f97f790d34c1d13bc59411c7
Button99 <56029580+Button99@users.noreply.github.com>
2024-07-27 11:14:00 +0300

    Fresh upload

--- CityInfo.API/CityInfo.API/obj/Release/net6.0/CityInfo.API.AssemblyInfo.cs
+++ CityInfo.API/CityInfo.API/obj/Release/net6.0/CityInfo.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3642fdd9f8eb42e07356ebe7fd65c11b6112bee3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
